62f710b06b90211973c2a3f90fb3fbba646a1985
AMOL29102 <your_email@example.com>
2025-01-29 16:20:25 +0530

    Videos5

--- Frontend/src/components/CameraModel.jsx
+++ Frontend/src/components/CameraModel.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useGLTF } from "@react-three/drei";
 
 const CameraModel = () => {
-  const { scene } = useGLTF("/nikon_d5600/scene.gltf"); // Replace with your actual file path
+  const { scene } = useGLTF("/Assets/nikon_d5600/scene.gltf"); // Replace with your actual file path
   return <primitive object={scene} />;
 };
 
